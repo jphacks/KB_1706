4bf48d53bdf39bf11a52f3717f9512599fd101b6
Ohira <tennmoku71@gmail.com>
2017-10-21 19:26:17 +0900

    puma sock

--- paycoin/config/puma.rb
+++ paycoin/config/puma.rb
@@ -4,16 +4,25 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+pidfile "/var/www/rails/paycoin/shared/tmp/pids/puma.pid"
+bind "unix:///var/www/rails/paycoin/shared/tmp/sockets/puma.sock"
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+state_path "/var/www/rails/paycoin/shared/tmp/pids/puma.state"
+stdout_redirect "/var/www/rails/paycoin/shared/log/puma.stdout.log", "/var/www/rails/paycoin/shared/log/puma.stderr.log", true
+
+environment ENV['RAILS_ENV'] 
+
+# threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# threads threads_count, threads_count
+
+# # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# #
+# port        ENV.fetch("PORT") { 3000 }
+
+# # Specifies the `environment` that Puma will run in.
+# #
+# environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
