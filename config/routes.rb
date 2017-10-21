bfd752a7a1d906e7e09fa38620eac2cf6c6a649d
Ohira <tennmoku71@gmail.com>
2017-10-21 23:14:11 +0900

    setting puma

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-
-  namespace :api do
-    mount_devise_token_auth_for 'User', at: 'auth'
-  end
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
