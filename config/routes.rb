ad6ac3d2cb9c796fb3ff72d094c183a3ec064136
Ohira <tennmoku71@gmail.com>
2017-10-21 15:59:38 +0900

    devise setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth'
+
+  namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
+
 end
