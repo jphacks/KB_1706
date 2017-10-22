4bf0ef5ec6eab9d7c7e2ec1328ea73a16d868b33
NishiYusuke <yuusuke8123@gmail.com>
2017-10-22 14:59:02 +0900

    Add files via upload

--- src/biometrix/biometrix/main.cpp
+++ src/biometrix/biometrix/main.cpp
@@ -21,18 +21,9 @@ using namespace cv;
 // Forward declarations of local functions for Private Pool Setup.
 
 HRESULT CaptureSample();
-//bool SaveBMP(BYTE* buffer, int width, int height, int pixelPerVertical, int pixelPerHorizontal, long paddedsize, LPCTSTR bmpfile, BYTE* firstPixel);
 bool SaveBMP(BYTE* Buffer, int width, int height, long paddedsize, LPCTSTR bmpfile);
 void thinningIte(Mat& img, int pattern);
 void thinning(const Mat& src, Mat& dst);
-
-/*
-int g_MeanBlockSize = 0;
-int g_MeanOffset = 0;
-int g_GaussianBlockSize = 0;
-int g_GaussianOffset = 0;
-*/
-//-----------------------------------------------------------------------------
 
 
 int main()
@@ -40,22 +31,32 @@ int main()
 	int Flag;
 	do {
 		HRESULT hr = S_OK;
-		hr = CaptureSample();
+		hr = CaptureSample();//指紋画像取得
 		wprintf_s(L"Capture Sample: hr = 0x%x\n", hr);
 
 		std::cin;
 		cv::Mat src_img = cv::imread("C:/Users/Yusuke/source/repos/biometrix/biometrix/fingerprint.bmp", 1);
+
+		// エラー処理
+		if (src_img.empty()) return -1;
+
 		cv::imwrite("src_img.bmp", src_img);
 		cv::Mat search_img, img_result,out;
 		src_img.copyTo(search_img);
 		src_img.copyTo(out);
 		out = Scalar(Vec3b(0, 0, 0));
-		Mat tmp[4];// テンプレート画像の取得
+		
 
-		int count[4] = { 0 };
+		// 特徴点描画用パレット
+		std::vector<cv::Vec3b> colors(4);
+		colors[0] = cv::Vec3b(0, 0, 255);
+		colors[1] = cv::Vec3b(0, 255, 0);
+		colors[2] = cv::Vec3b(255, 0, 0);
+		colors[3] = cv::Vec3b(0, 255, 255);
+
+		Mat tmp[4];// マニューシャ特徴量パターン
 		for (int i = 0; i < 4; i++)
-			tmp[i] = Mat(Size(3, 3), CV_8U, Scalar::all(0));// テンプレート画像の取得
-		//tmp[i].at<uchar>(ｙ, x);
+			tmp[i] = Mat(Size(3, 3), CV_8U, Scalar::all(0));// 初期化
 		tmp[0].at<uchar>(1, 1) = 255;
 		tmp[0].at<uchar>(2, 1) = 255;
 		tmp[1].at<uchar>(1, 1) = 255;
@@ -68,29 +69,21 @@ int main()
 		tmp[3].at<uchar>(1, 1) = 255;
 		tmp[3].at<uchar>(2, 1) = 255;
 		tmp[3].at<uchar>(2, 1) = 255;
+
+		// パターンを画像として保存
 		cv::imwrite("tmp1.jpg", tmp[0]);
 		cv::imwrite("tmp2.jpg", tmp[1]);
 		cv::imwrite("tmp3.jpg", tmp[2]);
 		cv::imwrite("tmp4.jpg", tmp[3]);
 
-		if (src_img.empty()) return -1;
+		cv::cvtColor(search_img, search_img, CV_BGR2GRAY);//　グレースケールに変換
+		search_img = ~search_img;// 背景を黒にするために反転
+		cv::imwrite("src_gray_img.bmp", search_img);// 元画像を保存
+		cv::imshow("src", search_img);// 元画像表示
 
-		/// 画像を表示するウィンドウ
-		// ウィンドウの名前，プロパティ
-		// CV_WINDOW_AUTOSIZE : ウィンドウサイズを画像サイズに合わせる
-		// CV_WINDOW_FREERATIO : ウィンドウのアスペクト比を固定しない
-		//cv::namedWindow("image1", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
-		cv::cvtColor(search_img, search_img, CV_BGR2GRAY);
-		search_img = ~search_img;
-		cv::imwrite("src_gray_img.bmp", search_img);
-		//medianBlur(search_img, search_img, 3);          // メディアンフィルタ
-		//cv::imwrite("Filter_img.bmp", search_img);
-		cv::imshow("src", search_img);
-
-		// ウィンドウ名でウィンドウを指定して，そこに画像を描画
-		//cv::imshow("image1", src_img);
-//		threshold(search_img, search_img, 0, 255, THRESH_BINARY | THRESH_OTSU);
+//		threshold(search_img, search_img, 0, 255, THRESH_BINARY | THRESH_OTSU);//固定値の2値化
 	
+		// 近傍の平均値に合わせて2値化
 		cv::adaptiveThreshold(search_img,                              // 8ビット，シングルチャンネルの入力画像．
 			search_img,
 			255,                              // 2値化画像を作成する際の０以外の色。ここでは[ 0, 255 ]で作成する.
@@ -99,23 +92,15 @@ int main()
 			23,                                // ピクセルの閾値を求めるために利用される近傍領域のサイズ．3, 5, 7, など．
 			0                                 // 平均または加重平均から引かれる定数.この値により maxValue の範囲が変わる.
 		);
+
+		// 2値化した画像を保存
 		cv::imwrite("binary_search_img.bmp", search_img);
-		//GaussianBlur(search_img, search_img, (3,3),0);
+		//GaussianBlur(search_img, search_img, (3,3),0);  //　ガウシアンフィルタ
 		//medianBlur(search_img, search_img, 3);          // メディアンフィルタ
 		//cv::imwrite("Filter_img.bmp", search_img);
-		cv::imshow("result", search_img);
+		cv::imshow("result", search_img);// 途中経過を表示
 		
-		//cv::Mat element(2, 2, CV_8U, cv::Scalar(1));
-		//cv::morphologyEx(search_img, search_img, MORPH_CLOSE, element, cv::Point(-1, -1), 1);
-		//cv::imshow("dilate", search_img);
-		
-		//thinning(search_img, search_img);
-		//cv::imwrite("thin_search_img.bmp", search_img);
-		//Canny(search_img, search_img, 50, 200);
-		//cv::imwrite("canny_img.bmp", search_img);
-
-		cv::imshow("result2", search_img);
-		//cv::imwrite("dilate_img.bmp", search_img);
+		cv::Mat element(2, 2, CV_8U, cv::Scalar(1));
 		
 		// ラベルごとのROIを得る(0番目は背景なので無視)
 		cv::Mat roiImg;
@@ -128,7 +113,7 @@ int main()
 		int num = 0;
 		int th = 2;
 		// ラベリング結果の描画
-		for (int i = 0; i < nLab; i++) {
+		for (int i = 1; i < nLab; i++) {
 			int *param = stats.ptr<int>(i);
 			int x = param[cv::ConnectedComponentsTypes::CC_STAT_LEFT];
 			int y = param[cv::ConnectedComponentsTypes::CC_STAT_TOP];
@@ -145,37 +130,15 @@ int main()
 			else {
 				roiRects.push_back(cv::Rect(x, y, width, height));
 				num++;
-				//cv::rectangle(src_img, roiRects.at(i - 1), cv::Scalar(0, 255, 0), 2);
 			}
 		}
 		cv::imwrite("openning_search_img.bmp", search_img);
-		cv::imshow("result3", search_img);
-
-		//thinning(search_img, search_img); 
-		cv::imshow("result4", search_img);
-		/*
-		cv::Mat e(2, 2, CV_8U, cv::Scalar(1));
-		cv::erode(search_img, search_img, e, cv::Point(-1, -1), 1);
-		cv::imwrite("mofo.bmp", search_img);
-		cv::dilate(search_img, search_img, e, cv::Point(-1, -1), 1);
-		cv::imwrite("dilate_img.bmp", search_img);
-
-		//thinning(search_img, search_img);
-		//cv::imwrite("thin_search_img.bmp", search_img);
-
-
-		cv::Mat element2(2, 2, CV_8U, cv::Scalar(1));
-		cv::dilate(search_img, search_img, element2, cv::Point(-1, -1), 2);
-		cv::imwrite("dilate_img2.bmp", search_img);
-		cv::erode(search_img, search_img, element2, cv::Point(-1, -1), 2);
-		cv::imwrite("mofo2.bmp", search_img);
-
-		*/
+		cv::imshow("result2", search_img);
 		cv::imwrite("search_img.bmp", search_img);
-		int N = 500, sum = 0;
-		double zx = 0, zy = 0;
+		int N = 500, sum = 0,count[4] = { 0 };
+		double zx[4] = { 0 }, zy[4] = { 0 };
 		// テンプレートマッチングで探索
-		for (int j = 0; j < 4; j++) {
+		for (int j = 1; j < 4; j++) {
 			cv::Mat &img_reso_koma = tmp[j];
 			int ox = -1, oy = -1;
 
@@ -185,28 +148,27 @@ int main()
 				Mat match;
 				cv::Point minp, maxp;
 				double minVal, maxVal;
-				//cv::matchTemplate(search_img, img_reso_koma, match, CV_TM_CCOEFF_NORMED);
 				cv::matchTemplate(search_img, img_reso_koma, match, CV_TM_SQDIFF);
 				cv::minMaxLoc(match, &minVal, &maxVal, &minp, &maxp);
 
 				// 一定スコア以下の場合は処理終了
 				//if (maxVal < 0.1) break;
-				if ((minVal > 0.01) || ((ox == minp.x) && (oy == minp.y))) {
+				if ((minVal > 0.001) || ((ox == minp.x) && (oy == minp.y))) {
 					i = N;
 					break;
 				}
 				else count[j]++;
 				ox = minp.x;
 				oy = minp.y;
-				zx += ox;
-				zy += oy;
+				zx[j] += ox;
+				zy[j] += oy;
 				//wprintf_s(L"\tmatch:%f\n", match);
 				//wprintf_s(L"\t%d:minVal%lf\n", i,minVal);
-				//
-				//wprintf_s(L"\tx:%d,y:%d\n", minp.x,minp.y);
+				//wprintf_s(L"\tx:%d,y:%d\n", minp.x,minp.y)
+
 				// 探索結果の場所に矩形を描画
-				circle(src_img, Point(minp.x, minp.y), 5, cv::Scalar(0, 0, 255), 1, 8, 0);
-				circle(out, Point(minp.x, minp.y), 5, cv::Scalar(0, 0, 255), 1, 8, 0);
+				circle(src_img, Point(minp.x, minp.y), 5, colors[j], 1, 8, 0);
+				circle(out, Point(minp.x, minp.y), 5, colors[j], 1, 8, 0);
 
 				// 検出済みは 0.0 で塗りつぶし
 				for (int y = 0; y < img_reso_koma.rows; y++) {
@@ -223,68 +185,52 @@ int main()
 			}
 		}
 		cv::imwrite("out.bmp", out);
-		cv::Mat src = cv::imread("C:/Users/Yusuke/source/repos/biometrix/biometrix/out.bmp");
-		cv::cvtColor(out, out, CV_BGR2GRAY);
-		cv::cvtColor(src, src, CV_BGR2GRAY);
 		cv::Mat dist = cv::imread("C:/Users/Yusuke/source/repos/biometrix/biometrix/search_img.bmp");
-		cv::Mat user_tmp = cv::imread("C:/Users/Yusuke/source/repos/biometrix/biometrix/search_img.bmp");
-		//cv::Mat user_tmp = cv::imread("C:/Users/Yusuke/source/repos/biometrix/biometrix/Ohiratmp.bmp");
-		cv::cvtColor(user_tmp, user_tmp, CV_BGR2GRAY);
-		// ユーザテンプレートマッチング
+		cv::Mat N_tmp = cv::imread("C:/Users/Yusuke/source/repos/biometrix/biometrix/Ntmp.bmp");
+		cv::Mat O_tmp = cv::imread("C:/Users/Yusuke/source/repos/biometrix/biometrix/Otmp.bmp");
 		
-		//std::cout << "Rect: " << roiRects.at(i - 1) << std::endl;
-		//cv::Mat roi = src(roiRects.at(i - 1));    // 対象とするブロブの領域取り出し
-		//cv::Mat roi;
-		// 切り抜く位置と大きさの指定はcv::Rectを用いる
-		//cv::Rect rect(0,0,user_tmp.rows, user_tmp.cols);
-		// 下記のようにcv::Matを作成すると，一部切り出せる
-		//cv::Mat imgSub(src, roi);
-		//double similarity = cv::matchShapes(user_tmp, roi, CV_CONTOURS_MATCH_I2, 0);// huモーメントによるマッチング
-		//std::cout << "Similarity: " << similarity << std::endl;
-
-		for (int i = 0; i < 4; i++) {
+		for (int i = 1; i < 4; i++) {
+			zx[i] = zx[i] / count[i];
+			zy[i] = zy[i] / count[i];
 			sum += count[i];
 			wprintf_s(L"\tcount%d:%d\n", i, count[i]);
+			std::cout << "重心"<<i<<":(" << zx[i] << "," << zy[i] << ") " << std::endl;
 		}
 		double ave = (double)sum / 4;
 		wprintf_s(L"\tcount ave:%f\n", ave);
-		std::cout << "重心:(" << zx / sum << "," << zy / sum << ") " << std::endl;
-
+		
+		// ユーザテンプレートマッチング
 		// 最大のスコアの場所を探す
-		/*
 		Mat match;
 		cv::Point minp, maxp;
 		double minVal, maxVal;
-		//cv::matchTemplate(search_img, img_reso_koma, match, CV_TM_CCOEFF_NORMED);
-		cv::matchTemplate(src, user_tmp, match, CV_TM_CCOEFF_NORMED);
+		
+		cv::matchTemplate(out, N_tmp, match, CV_TM_CCOEFF_NORMED);
 		cv::minMaxLoc(match, &minVal, &maxVal, &minp, &maxp);
-		// 一定スコア以下の場合は処理終了
-		//if (maxVal < 0.1) break;
 		std::cout << "Val: " << maxVal << ",x: " << maxp.x << ",y: " << maxp.y << std::endl;
 		circle(dist, Point(maxp.x, maxp.y), 5, cv::Scalar(0, 0, 255), 1, 8, 0);
-		if (maxVal > 0.6) {
-			std::cout << "Match!! " << std::endl;
-		}
-		else std::cout << "No Match " << std::endl;
-		*/
-		if (ave > 30) {
-			std::cout << "Nishimura " << std::endl;
-		}
-		else std::cout << "Ohira" << std::endl;
-		cv::imwrite("result.bmp", src_img);
 		
-		cv::imshow("result5", src_img);
+		// 一定スコア以下の場合は処理終了
+		if (maxVal > 0.1) {
+			std::cout << "Nishimura" << std::endl;
+		}
+		else {
+			std::cout << "Ohira" << std::endl;
+		}
+		cv::imwrite("result.bmp", dist);
+		
+		cv::imshow("result3", dist);
 		cv::imshow("output", out);
-		cv::imshow("user_tmp", user_tmp);
+		cv::imshow("N_tmp", N_tmp);
+		cv::imshow("O_tmp", O_tmp);
 		cv::waitKey(0);
 		// キー入力を（無限に）待つ
 		cvDestroyWindow("src");
 		cvDestroyWindow("result");
 		cvDestroyWindow("result2");
 		cvDestroyWindow("result3");
-		cvDestroyWindow("result4");
-		cvDestroyWindow("result5");
-		cvDestroyWindow("user_tmp");
+		cvDestroyWindow("N_tmp");
+		cvDestroyWindow("O_tmp");
 		cvDestroyWindow("output");
 		wprintf_s(L"\n Press 1 to repeat.\n Press 0 to exit\n. ");
 		std::cin >> Flag;
@@ -343,29 +289,6 @@ restart:
 		wprintf_s(L"\n[%d]: \tUnit ID: %d\n",
 			index,
 			unitSchema[index].UnitId);
-		/*
-		wprintf_s(L"\tDevice instance ID: %s\n",
-			unitSchema[index].DeviceInstanceId);
-		wprintf_s(L"\tPool type: %d\n",
-			unitSchema[index].PoolType);
-		wprintf_s(L"\tBiometric factor: %d\n",
-			unitSchema[index].BiometricFactor);
-		wprintf_s(L"\tSensor subtype: %d\n",
-			unitSchema[index].SensorSubType);
-		wprintf_s(L"\tSensor capabilities: 0x%08x\n",
-			unitSchema[index].Capabilities);
-		wprintf_s(L"\tDescription: %s\n",
-			unitSchema[index].Description);
-		wprintf_s(L"\tManufacturer: %s\n",
-			unitSchema[index].Manufacturer);
-		wprintf_s(L"\tModel: %s\n",
-			unitSchema[index].Model);
-		wprintf_s(L"\tSerial no: %s\n",
-			unitSchema[index].SerialNumber);
-		wprintf_s(L"\tFirmware version: [%d.%d]\n",
-			unitSchema[index].FirmwareVersion.MajorVersion,
-			unitSchema[index].FirmwareVersion.MinorVersion);
-		*/
 	}
 
 	wprintf_s(L"\n Pick a Sensor: ");
@@ -398,38 +321,16 @@ restart:
 		goto e_Exit;
 	}
 
-	//wprintf_s(L"\n Swipe processed - Unit ID: %d\n", unitId);
-	//wprintf_s(L"\n Captured %d bytes.\n", sampleSize);
-	//wprintf_s(L"\n Captured %d bytes.\n", sample);
-
 	PWINBIO_BIR_HEADER BirHeader = (PWINBIO_BIR_HEADER)(((PBYTE)sample) + sample->HeaderBlock.Offset); //header points to the offset of the header block
 	PWINBIO_BDB_ANSI_381_HEADER AnsiBdbHeader = (PWINBIO_BDB_ANSI_381_HEADER)(((PBYTE)sample) + sample->StandardDataBlock.Offset); //header points to the beginning of the standard data block
 	PWINBIO_BDB_ANSI_381_RECORD AnsiBdbRecord = (PWINBIO_BDB_ANSI_381_RECORD)(((PBYTE)AnsiBdbHeader) + sizeof(WINBIO_BDB_ANSI_381_HEADER)); //record points to the record of the standard data block
 
 	PBYTE firstPixel = (PBYTE)((PBYTE)AnsiBdbRecord) + sizeof(WINBIO_BDB_ANSI_381_RECORD); //points to the data of first pixel
 
-	//wprintf_s(L"\n ID: %d\n", AnsiBdbHeader->ProductId.Owner);
-	//wprintf_s(L" Horizontal Img. Res.: %d\n", AnsiBdbHeader->HorizontalImageResolution);
-	//wprintf_s(L" Horizontal Scan Img. Res.: %d pixels/centimeter\n", AnsiBdbHeader->HorizontalScanResolution);
-	//wprintf_s(L" Vertical Img. Res.: %d\n", AnsiBdbHeader->VerticalImageResolution);
-	//wprintf_s(L" Vertical Scan Img. Res.: %d pixels/centimeter\n", AnsiBdbHeader->VerticalScanResolution);
-	//wprintf_s(L" Pixel Depth: %d\n", AnsiBdbHeader->PixelDepth); //number of bits in one pixel
-	//wprintf_s(L" Element Count: %d\n", AnsiBdbHeader->ElementCount); //number of images
-
 	wprintf_s(L" Width: %d\n", AnsiBdbRecord->HorizontalLineLength);
 	wprintf_s(L" Height: %d\n", AnsiBdbRecord->VerticalLineLength);
-	//wprintf_s(L" Blocklength: %d\n", AnsiBdbRecord->BlockLength);
-	//wprintf_s(L" Compression Algorithm: %d\n", AnsiBdbHeader->ImageCompressionAlg);
-
-	//wprintf_s(L" Address of First Pixel in HEX: 0x%x\n", firstPixel);
-	//wprintf_s(L" Address of First Pixel in DEC: %d\n", firstPixel);
-	//wprintf_s(L" WINBIO_BDB_ANSI_381_RECORD: %d\n", sizeof(AnsiBdbRecord));
-
-	//AnsiBdbRecord->BlockLength is size of data + record header
+	
 	long blocklength = AnsiBdbRecord->BlockLength - sizeof(AnsiBdbRecord); //blocklength is size of raw image data
-																		   //long size = sizeof(char) * 4;
-
-	//bool b = SaveBMP(firstPixel, AnsiBdbRecord->HorizontalLineLength, AnsiBdbRecord->VerticalLineLength, AnsiBdbRecord->VerticalLineLength, AnsiBdbRecord->HorizontalLineLength, blocklength, "fingerprint.bmp", firstPixel);
 	bool b = SaveBMP(firstPixel, AnsiBdbRecord->HorizontalLineLength, AnsiBdbRecord->VerticalLineLength, blocklength, "fingerprint.bmp");
 
 	if (b) {
@@ -491,12 +392,12 @@ bool SaveBMP(BYTE* Buffer, int width, int height, long paddedsize, LPCTSTR bmpfi
 	info.biCompression = 0;
 
 	info.biSizeImage = 0;
-	/*
+	
 	info.biXPelsPerMeter = 0x0ec4;
 	info.biYPelsPerMeter = 0x0ec4;
-	*/
-	info.biXPelsPerMeter = 500;
-	info.biYPelsPerMeter = 500;
+
+	//info.biXPelsPerMeter = 500;
+	//info.biYPelsPerMeter = 500;
 	info.biClrUsed = 0;
 
 	info.biClrImportant = 0;
