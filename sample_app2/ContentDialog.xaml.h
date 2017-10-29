a1a338b62918fb07336a846c43d73a50a94eea2a
Arashi S <arashi.sako9@gmail.com>
2017-10-29 21:05:27 +0900

    add vb file

---
+++ sample_app2/ContentDialog.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// ContentDialog.xaml.h
+// ContentDialog クラスの宣言
+//
+
+#pragma once
+
+#include "ContentDialog.g.h"
+
+namespace sample_app2
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class ContentDialog sealed
+	{
+	public:
+		ContentDialog();
+	private:
+		void ContentDialog_PrimaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ args);
+		void ContentDialog_SecondaryButtonClick(Windows::UI::Xaml::Controls::ContentDialog^ sender, Windows::UI::Xaml::Controls::ContentDialogButtonClickEventArgs^ args);
+	};
+}
